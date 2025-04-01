e0f8f5d71f53a71fc0889ca55a7a59a893bf414f
madortill <mador.till@gmail.com>
2025-04-01 13:26:01 +0300

    chack

--- src/components/TypesOfCranes.vue
+++ src/components/TypesOfCranes.vue
@@ -2,35 +2,41 @@
   <div id="types-of-cranes">
     <p class="header">{{ title }}</p>
 
-    <crane-carousel :style="{ display: !cardClicked ? 'block' : 'none' }" @change-title="changeTitle" />
-    <div  :style="{ display: cardClicked ? 'block' : 'none' }" >
-      <origin-crane-name v-if="partLearningCraneCard === 0" :craneKind="clickedCrane"></origin-crane-name>
-      <crane-operations v-if="partLearningCraneCard === 1" :craneKind="clickedCrane"></crane-operations>
+    <crane-carousel
+      :style="{ display: !cardClicked ? 'block' : 'none' }"
+      @change-title="changeTitle"
+    />
+    <div :style="{ display: cardClicked ? 'block' : 'none' }">
+      <origin-crane-name
+        v-if="partLearningCraneCard === 0"
+        :craneKind="clickedCrane"
+      ></origin-crane-name>
+      <crane-operations
+        v-if="partLearningCraneCard === 1"
+        :craneKind="clickedCrane"
+      ></crane-operations>
     </div>
-    
   </div>
 </template>
 
 <script>
 import CraneCarousel from "./CraneCarousel.vue";
-import CraneOperations from './CraneOperations.vue';
-import OriginCraneName from './OriginCraneName.vue';
-
+import CraneOperations from "./CraneOperations.vue";
+import OriginCraneName from "./OriginCraneName.vue";
 
 export default {
-  components: { CraneCarousel , OriginCraneName , CraneOperations },
+  components: { CraneCarousel, OriginCraneName, CraneOperations },
   name: "types-of-cranes",
-  props: ['cardClicked', 'title', 'partLearningCraneCard'],
+  props: ["cardClicked", "title", "partLearningCraneCard"],
   data() {
     return {
-      clickedCrane: '',
-
+      clickedCrane: "",
     };
   },
   methods: {
     changeTitle(craneTitle) {
       this.clickedCrane = craneTitle;
-      this.$emit('crane-card-chosen', craneTitle);
+      this.$emit("crane-card-chosen", craneTitle);
     },
   },
 };
@@ -39,12 +45,12 @@ export default {
 <style scoped>
 #types-of-cranes {
   width: 100vw;
-    height: 77vh;
-    margin-top: 5rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  height: 77vh;
+  margin-top: 5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 
 .header {
@@ -53,5 +59,8 @@ export default {
   /* padding: 2rem; */
 }
 @media screen and (max-width: 700px) {
+  #types-of-cranes {
+    margin-top: 2rem;
+  }
 }
 </style>
