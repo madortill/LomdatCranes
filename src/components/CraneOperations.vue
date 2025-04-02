4e39057a71af16b83d3160681f9a0aad230be52a
madortill <mador.till@gmail.com>
2025-04-02 09:02:15 +0300

    with no pop out animation to orange bubble

--- src/components/CraneOperations.vue
+++ src/components/CraneOperations.vue
@@ -2,15 +2,56 @@
   <div id="crane-operations">
     <p class="header">פעולות העגורן</p>
     <div class="bubble-container">
-      <div v-for="(info, index) in arrinfo" :key="index" class="bubble">
+      <div
+        v-for="(info, index) in arrinfo"
+        :key="index"
+        class="bubble"
+        :class="
+          index === 2 && isClickedQuestionMark && this.craneKind === 'עגורן שער'
+            ? 'more-info-bubble'
+            : ''
+        "
+      >
         <p class="the-info">{{ info }}</p>
-        <div  class="orange-bubble"  v-if="index === 2 && this.craneKind === 'עגורן שער'" >
-          <p class="question-mark">?</p>
+
+        <div
+          :class="
+            index === 2 &&
+            isClickedQuestionMark &&
+            this.craneKind === 'עגורן שער'
+              ? 'more-info'
+              : 'hide'
+          "
+        >
+          <hr class="line" />
+          העגורן נע על מסילות חשמליות מה שמאפשר להזיז את קורת העגורן אחורה
+          וקדימה לכל אורך שטח המסילות.
+        </div>
+        <div
+          @click="showMoreInfo"
+          class="orange-bubble"
+          v-if="index === 2 && this.craneKind === 'עגורן שער'"
+        >
+          <p
+            class="question-mark icon-orange-bubble"
+            v-if="!isClickedQuestionMark"
+          >
+            ?
+          </p>
+          <p class="icon-orange-bubble" v-else>X</p>
         </div>
       </div>
     </div>
     <div class="numbers-container">
-        <p class="num" v-for="num in numArr" :key="num">{{ num }}</p>
+      <p
+        v-if="!isClickedQuestionMark"
+        class="num"
+        v-for="num in numArr"
+        :key="num"
+      >
+        {{ num }}
+      </p>
+      <p v-if="isClickedQuestionMark" class="num">3</p>
     </div>
   </div>
 </template>
@@ -21,7 +62,8 @@ export default {
   props: ["craneKind"],
   data() {
     return {
-        numArr: [1,2,3],
+      numArr: [1, 2, 3],
+      isClickedQuestionMark: false,
     };
   },
   computed: {
@@ -43,19 +85,21 @@ export default {
       return arr;
     },
   },
+  methods: {
+    showMoreInfo() {
+      if (this.isClickedQuestionMark) {
+        this.isClickedQuestionMark = false;
+      } else {
+        this.isClickedQuestionMark = true;
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
 #crane-operations {
-  /* display: flex; */
-  /* margin-top: 1rem; */
-  /* background-color: white; */
   margin-top: -2rem;
-  /* width: 55vw; */
-  /* height: 25rem; */
-  /* align-items: center;
-    justify-content: space-between; */
 }
 
 .the-info {
@@ -75,7 +119,9 @@ export default {
   width: 25rem;
   position: relative;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
 }
 
 .bubble-container {
@@ -86,15 +132,24 @@ export default {
 }
 
 .numbers-container {
-    position: absolute;
-    top: 15rem;
+  position: absolute;
+  top: 15rem;
 }
 
+.hide {
+  display: none;
+}
+
+.more-info {
+  display: block;
+}
+
+
 .num {
-    font-size: 3rem;
-    font-family: Secular-One;
-    margin-top: 2.6rem;
-    margin-bottom: 0rem;
+  font-size: 3rem;
+  font-family: Secular-One;
+  margin-top: 2.6rem;
+  margin-bottom: 0rem;
 }
 
 .orange-bubble {
@@ -106,13 +161,24 @@ export default {
   height: 5rem;
   border-radius: 100%;
   /* z-index: 100; */
+  cursor: pointer;
 }
 
-.question-mark {
+.more-info-bubble {
+  height: 20rem;
+  position: absolute;
+  /* transition: all 1s; */
+}
+
+.icon-orange-bubble {
   font-weight: bold;
   font-size: 2rem;
   margin-left: 1rem;
   margin-top: 1.5rem;
+  font-family: Secular-One;
+}
+
+.question-mark {
   animation: jump 1s ease-in-out infinite;
 }
 
@@ -132,5 +198,12 @@ export default {
   100% {
     transform: translateY(0); /* Return to original position */
   }
+}
+
+.line {
+  width: 10rem;
+  border: none;
+  border-top: 0.2rem solid black; /* Adjust the thickness of the line */
+  margin-bottom: 7rem;
 }
 </style>
