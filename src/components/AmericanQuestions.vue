244409749584ede8d8c809e68a6f25a6e23b1955
madortill <mador.till@gmail.com>
2025-04-02 18:58:24 +0300

    im ok

--- src/components/AmericanQuestions.vue
+++ src/components/AmericanQuestions.vue
@@ -1,37 +1,40 @@
 <template>
   <div id="american-questions" @click="checkAnswer">
-      <h1 class="title-questionMultiple">{{ this.questions[this.numQues].title }}</h1>
-      <h2 class="second-instraction"  v-if="this.questions[this.numQues].Qtype === 1">בחר/י בשלוש התשובות הנכונות.</h2>
-      <div class="div-mulQ">
-        <!-- <button v-for="i in 3" :key="i" :id="i" :ref="button${i}" class="pulse-button-hover"> {{ questions['ans' + i] }}</button> -->
+    <h1 class="title-questionMultiple">
+      {{ this.questions[this.numQues].title }}
+    </h1>
+    <h2
+      class="second-instraction"
+      v-if="this.questions[this.numQues].Qtype === 1"
+    >
+      בחר/י בשלוש התשובות הנכונות.
+    </h2>
+    <div class="div-mulQ">
+      <!-- <button v-for="i in 3" :key="i" :id="i" :ref="button${i}" class="pulse-button-hover"> {{ questions['ans' + i] }}</button> -->
 
-        <div class="row">
-          <button id="1" ref="1" class="pulse-button-hover">
-            {{ this.questions[this.numQues].ans1 }}
-          </button>
-          <button id="2" ref="2" class="pulse-button-hover">
-            {{ this.questions[this.numQues].ans2 }}
-          </button>
-        </div>
-        <div class="row">
-          <button id="3" ref="3" class="pulse-button-hover">
-            {{ this.questions[this.numQues].ans3 }}
-          </button>
-          <button id="4" ref="4" class="pulse-button-hover">
-            {{ this.questions[this.numQues].ans4 }}
-          </button>
-        </div>
+      <div class="row">
+        <button id="1" ref="1" class="pulse-button-hover">
+          {{ this.questions[this.numQues].ans1 }}
+        </button>
+        <button id="2" ref="2" class="pulse-button-hover">
+          {{ this.questions[this.numQues].ans2 }}
+        </button>
       </div>
-      <button
-        class="moving-btn next-btn"
-        v-if="showNextBtn"
-        @click="nextPart"
-      >
-        הבא
-      </button>
-
-      <button class="moving-btn back-btn" @click="backToInfo">חזור</button>
+      <div class="row">
+        <button id="3" ref="3" class="pulse-button-hover">
+          {{ this.questions[this.numQues].ans3 }}
+        </button>
+        <button id="4" ref="4" class="pulse-button-hover">
+          {{ this.questions[this.numQues].ans4 }}
+        </button>
+      </div>
     </div>
+    <button class="moving-btn next-btn" v-if="showNextBtn" @click="nextPart">
+      הבא
+    </button>
+
+    <button class="moving-btn back-btn" @click="backToInfo">חזור</button>
+  </div>
 </template>
 
 <script>
@@ -40,8 +43,8 @@ export default {
   // props: ['questions'],
   data() {
     return {
-        numQues: 0,
-        showNextBtn: false,
+      numQues: 0,
+      showNextBtn: false,
       arrayChosenCorrect: ["", "", ""],
       questions: [
         {
@@ -93,18 +96,27 @@ export default {
         //בדיקה אם הכפתור הנלחץ הוא תשובה נכונה
         if (
           (this.questions[this.numQues].Qtype === 1 &&
-            this.isInTheArray(this.questions[this.numQues].correctAnswer, event.target.id)) ||
+            this.isInTheArray(
+              this.questions[this.numQues].correctAnswer,
+              event.target.id
+            )) ||
           (this.questions[this.numQues].Qtype !== 1 &&
-            String(event.target.id) === String(this.questions[this.numQues].correctAnswer))
+            String(event.target.id) ===
+              String(this.questions[this.numQues].correctAnswer))
         ) {
           event.target.classList.add("correct");
           //בודק אם זאת שאלה עם תשובה אחת (שנכונה) או עם 3 תשובות שנלחצו ונכונות
           if (
-            (this.questions[this.numQues].Qtype === 1 && this.getNumCorrect() === 3) ||
+            (this.questions[this.numQues].Qtype === 1 &&
+              this.getNumCorrect() === 3) ||
             this.questions[this.numQues].Qtype !== 1
           ) {
             setTimeout(() => {
-              for (let i = 1; i <= this.questions[this.numQues].numAnswer; i++) {
+              for (
+                let i = 1;
+                i <= this.questions[this.numQues].numAnswer;
+                i++
+              ) {
                 if (this.$refs[i].classList.contains("correct")) {
                   this.$refs[i].classList.remove("correct");
                 }
@@ -112,7 +124,7 @@ export default {
                   this.$refs[i].classList.remove("wrong");
                 }
               }
-              if(this.numQues !== 3) {
+              if (this.numQues !== 3) {
                 this.numQues++;
               } else {
                 this.showNextBtn = true;
@@ -146,7 +158,7 @@ export default {
       // this.part++;
       this.$emit("next-part");
     },
-  
+
     backToInfo() {
       this.$emit("back-to-info");
     },
@@ -243,7 +255,7 @@ export default {
 
 .div-mulQ {
   width: 40rem;
-  height: 20rem;
+  /* height: 20rem; */
   background: #fff;
   border-radius: 3rem;
   box-shadow: 0 15px 20px -20px rgba(0, 0, 0, 0.4);
@@ -311,6 +323,12 @@ export default {
       rgba(255, 255, 255, 0) 50%,
       rgba(255, 255, 255, 0.4) 100%
     );
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .div-mulQ {
+    width: 30rem;
   }
 }
 </style>
