db2eff64c622ca98070eb5bad67c1223cf8831d1
madortill <mador.till@gmail.com>
2025-06-29 17:42:03 +0300

    rrr

--- src/components/CraneComponents.vue
+++ src/components/CraneComponents.vue
@@ -387,10 +387,10 @@ export default {
 
     .to-orange-zoom {
       margin-top: -148rem;
-      margin-left: 175.7rem;
+      margin-left: 170.7rem;
       transition: transform 1s ease-in, width 1s ease-in, margin-top 1s ease-in,  margin-bottom 1s ease-in
         margin-left 1s ease-in;
-      margin-bottom: -122rem;
+      margin-bottom: -112rem;
       width: 263rem;
     }
 
