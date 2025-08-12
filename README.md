a94c905b94e11ad9d43dd4323a6a7d7183040f5e
madortill <mador.till@gmail.com>
2025-08-12 19:04:36 +0300

    now

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 **Tip🤩**
-     You have the order of the components of the project in **the assets file**
-     
+   -  You have the order of the components of the project in **the assets file**
+   - in the campus you have the lomda- the Scrom file is in the MadorTill archive--> proj archive --> Bahad 20 --> 2024
+   - in the campus i added a component of FullScreenBtn - because the resolution --> you can see the component in the assets. when you add- add it to the App component with v-if="partNum === 0"
    *Dani Shriky 2025*
---
+++ src/assets/FullScreenBtn.vue
@@ -0,0 +1,54 @@
+<template>
+    <button @click="toggleFullscreen" class="fullscreen-btn">
+      ⛶ מסך מלא
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    name: "fuul-screen-btn",
+    methods: {
+      toggleFullscreen() {
+        const elem = document.documentElement; // the whole page
+  
+        if (!document.fullscreenElement) {
+          if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+          } else if (elem.webkitRequestFullscreen) { // Safari
+            elem.webkitRequestFullscreen();
+          } else if (elem.msRequestFullscreen) { // IE11
+            elem.msRequestFullscreen();
+          }
+        } else {
+          if (document.exitFullscreen) {
+            document.exitFullscreen();
+          } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+          } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+          }
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .fullscreen-btn {
+    position: fixed;
+    top: 10px;
+    right: 10px;
+    background: #333;
+    color: white;
+    border: none;
+    padding: 8px 14px;
+    cursor: pointer;
+    z-index: 9999;
+    border-radius: 4px;
+    font-size: 14px;
+  }
+  .fullscreen-btn:hover {
+    background: #555;
+  }
+  </style>
+  
